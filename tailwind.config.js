afb650614b6951e9e51d9ea9983517d5797e5b49
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-08-17 18:48:58 +0500

    :apple: tailwind and prettier configured

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "320px",
+        mm: "375px",
+        ml: "425px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+        "2xl": "2560px",
+      },
+      // not sure about adding fallback for dvh like this
+      height: {
+        screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+      },
+      colors: {
+        primary: "var(--primary-color)",
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+}
