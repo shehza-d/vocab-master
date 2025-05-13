b2c717445f4f4105d4c604ee585d484b0aa0d74f
shehzad iqbal <shehzaddiqbal@gmail.com>
2025-05-13 21:37:00 +0500

    exercise number functionality

--- src/components/Arabic.tsx
+++ src/components/Arabic.tsx
@@ -4,10 +4,64 @@ import { IWord } from "../types";
 import "./button.css";
 
 export default function Arabic() {
+  const [selectedExercise, setSelectedExercise] = useState<string>("all");
   const [currentWord, setCurrentWord] = useState<IWord>(words[0]);
   const [isWordVisible, setIsWordVisible] = useState(false);
+
+  const filteredWords =
+    selectedExercise === "all"
+      ? words
+      : words.filter(
+          (word) => word?.exerciseNumber?.toString() === selectedExercise,
+        );
+
+  const getRandomWord = () => {
+    const randomIndex = Math.floor(Math.random() * filteredWords.length);
+    return filteredWords[randomIndex];
+  };
+
+  const exerciseNumbers = [
+    ...new Set(
+      words
+        .map((word) => word.exerciseNumber)
+        .filter((num): num is number => typeof num === "number"),
+    ),
+  ].sort((a, b) => a - b);
+
   return (
     <div className="center flex h-screen w-full max-w-96 flex-col gap-8 text-5xl">
+      <div className="flex w-64 items-center justify-around">
+        <label htmlFor="options" className="text-2xl font-medium text-gray-700">
+          Exercise
+        </label>
+        <select
+          id="options"
+          name="options"
+          value={selectedExercise}
+          onChange={(e) => {
+            setSelectedExercise(e.target.value);
+            const newWordList =
+              e.target.value === "all"
+                ? words
+                : words.filter(
+                    (w) => w?.exerciseNumber?.toString() === e.target.value,
+                  );
+            setCurrentWord(
+              newWordList[Math.floor(Math.random() * newWordList.length)],
+            );
+            setIsWordVisible(false);
+          }}
+          className="focus:ring-primary focus:border-priring-primary block rounded-md border border-gray-300 bg-white px-4 py-2 text-sm text-gray-700 shadow-sm focus:ring-2 focus:outline-none"
+        >
+          <option value="all">All</option>
+          {exerciseNumbers.map((exerciseNumber, i) => (
+            <option key={`${exerciseNumber}${i}`} value={exerciseNumber}>
+              {exerciseNumber}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <p className="font-urdu">{currentWord.arabic}</p>
 
       <div
@@ -21,8 +75,7 @@ export default function Arabic() {
       <button
         className="my-button w-36!"
         onClick={() => {
-          const randomIndex = Math.floor(Math.random() * words.length);
-          setCurrentWord(words[randomIndex]);
+          setCurrentWord(getRandomWord());
           setIsWordVisible(false);
         }}
       >
